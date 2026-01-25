9f296cdca3b1d02bff1f741bbdc91d21be237db2
Sourav <godmakedit04@gmail.com>
2026-01-25 11:25:27 +0530

    Minor Improvement

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,25 @@ import { useState } from "react";
 function App() {
 	const [number, setNumber] = useState("");
 	const isInvalid = number.length != 10 && isNaN(Number(number));
+
 	const isFancy =
 		!number.split("").some((n) => {
 			return ["2", "4", "8"].includes(n);
 		}) &&
 		(number.endsWith("5") || number.endsWith("55"));
+
+	function sumCalculator(num) {
+		num = Math.abs(Number(num));
+
+		if (num < 10) return num;
+
+		const sum = num
+			.toString()
+			.split("")
+			.reduce((acc, digit) => acc + Number(digit), 0);
+
+		return sumCalculator(sum);
+	}
 
 	return (
 		<div className="w-screen h-screen flex items-center justify-center flex-col gap-4">
@@ -22,7 +36,7 @@ function App() {
 				<p className="text-red-400">Enter a Valid Number</p>
 			)}
 			{number.length != 0 && !isInvalid && (
-				<h4>{`The Sum is ${number.split("").reduce((acc, currNum) => acc + Number(currNum), 0)}`}</h4>
+				<h4>{`The Sum is ${sumCalculator(number)}`}</h4>
 			)}
 
 			{number.length == 10 && !isInvalid && isFancy && (
